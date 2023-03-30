379c1320a8e1cbe702b5ed598d3c1647b509dba3
PavelGogulya <PavelGogulya@example.com>
2023-03-30 09:14:24 +0300

    Создали программу в Task_00

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Написать прогу, которая 
+1 на вход принимает число и 
+2  выдает его квадрат
+4 -> 16
+-3 -> 9
+-7 -> 49 */
+
+
+Console.WriteLine("Введите целое число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+/* ЛИБО string str = Console.ReadLine();
+int number = Convert.ToInt32(str); */
+
+int squareNumber = number * number;
+Console.WriteLine($"Квадрат числа {number} = {squareNumber}");
+
