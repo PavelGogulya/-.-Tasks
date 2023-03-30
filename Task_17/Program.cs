0fd4b8915f06727daa26b85e517806495a878014
PavelGogulya <PavelGogulya@example.com>
2023-03-30 20:01:29 +0300

    create Task_17/. Создали задачу 17

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -3,17 +3,47 @@
 номер четверти плоскости, в которой находится эта
 точка. */
 
+// /* Console.WriteLine("Введите координаты точки");
+// Console.Write("X: ");
+// int xCoordinate = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y: ");
+// int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+// int quarter = Quarter(xCoordinate, yCoordinate);
+// string result = quarter > 0
+//                 ? $"Указанные координаты соответствуют четверти -> {quarter}" 
+//                 : "Введены некорректные координаты";
+
+// Console.WriteLine(result);
+
+// int Quarter (int x, int y) // цельночисленный тип данных и имя "четверть" - quarter
+// {
+//     if(x > 0 && y > 0) return 1;
+//     if(x < 0 && y > 0) return 2;
+//     if(x < 0 && y < 0) return 3;
+//     if(x > 0 && y < 0) return 4;
+//     return 0;
+// } */
+
 Console.WriteLine("Введите координаты точки:");
 Console.WriteLine("x: ");
 int xCoordinate = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("y: ");
 int yCoordinate = Convert.ToInt32(Console.ReadLine());
 
-int Quarter (int x, int y)
+int quarter = Quarter(xCoordinate, yCoordinate); //переменная цельночисленного типа = вызываем метод с (фактическими парамерами)
+string result = quarter > 0
+            ?$"Указанные координаты соответствуют четверти -> {quarter}":
+             "Введены некорректные координаты";
+
+Console.WriteLine(result);
+
+int Quarter(int x, int y)
+
 {
-    if(x > 0 && y > 0) return 1;
-    if(x < 0 && y > 0) return 1;
-    if(x < 0 && y < 0) return 1;
-    if(x > 0 && y < 0) return 1;
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
     return 0;
 }
