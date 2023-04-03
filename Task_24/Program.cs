f74a121df36cc9334fed9b6185383e12f919f7d1
PavelGogulya <PavelGogulya@example.com>
2023-04-03 20:11:37 +0300

    Создали программу для задачи Task_24

---
+++ Task_24/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+
+// Булгаков Андрей
+
+// Console.WriteLine("Введите целое положительное число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int result = SumNumbers(number);
+// Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
+
+// int SumNumbers(int num) // int num = number
+// {
+//     int sum = 0;
+//     for(int i = 1; i <= num; i++)
+//     {
+//         sum = sum + i; // sum += i/ Хотя фигурные скобки '{}' можем опустить, поскольку одно действие
+//     }
+//     return sum;
+// }
+
+
+//                  Игорь
+
