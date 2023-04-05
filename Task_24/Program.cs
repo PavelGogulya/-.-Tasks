976914592bbaf8cf0a14e330b030b2b5c742cd15
PavelGogulya <PavelGogulya@example.com>
2023-04-05 07:19:35 +0300

    Программа для задач 24 (сумма и факториал числа)

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -4,38 +4,38 @@
 
 // Булгаков Андрей
 
-Console.WriteLine("Введите целое положительное число");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите целое положительное число");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-int result = SumNumbers(number);
-Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
+// int result = SumNumbers(number);
+// Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
 
-int SumNumbers(int num) // int num = number
-{
-    int sum = 0;
-    for(int i = 1; i <= num; i++)
-    {
-        sum = sum + i; // sum += i/ Хотя фигурные скобки '{}' можем опустить, поскольку одно действие
-    }
-    return sum;
-}
-
-
-//                  Игорь
-
-// Console.WriteLine($"Введите целое положительное число A:");
-
-// int numberA = Convert.ToInt32(Console.ReadLine());
-
-// int result = Factorial(numberA);
-// Console.WriteLine($"Результат вычисления факториала: {result}");
-
-// int Factorial(int num) // int num = number
+// int SumNumbers(int num) // int num = number
 // {
-//     int resultFactorial = 1;
+//     int sum = 0;
 //     for(int i = 1; i <= num; i++)
 //     {
-//         resultFactorial *= i; // sum += i/ Хотя фигурные скобки '{}' можем опустить, поскольку одно действие
+//         sum = sum + i; // sum += i/ Хотя фигурные скобки '{}' можем опустить, поскольку одно действие
 //     }
-//     return resultFactorial;
+//     return sum;
 // }
+
+
+//                  Игорь. Поиск Факториала
+
+Console.WriteLine($"Введите целое положительное число A:");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int result = Factorial(numberA);
+Console.WriteLine($"Результат вычисления факториала: {result}");
+
+int Factorial(int num) // int num = number
+{
+    int resultFactorial = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        resultFactorial *= i; // sum += i/ Хотя фигурные скобки '{}' можем опустить, поскольку одно действие
+    }
+    return resultFactorial;
+}
