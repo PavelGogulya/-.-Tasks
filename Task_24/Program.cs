766c56bca93af3de8cb33dc808d83a28fb2e98f9
PavelGogulya <PavelGogulya@example.com>
2023-04-05 06:30:04 +0300

    Поправили программу для Task_24

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -4,21 +4,21 @@
 
 // Булгаков Андрей
 
-// Console.WriteLine("Введите целое положительное число");
-// int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
 
-// int result = SumNumbers(number);
-// Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
+int result = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
 
-// int SumNumbers(int num) // int num = number
-// {
-//     int sum = 0;
-//     for(int i = 1; i <= num; i++)
-//     {
-//         sum = sum + i; // sum += i/ Хотя фигурные скобки '{}' можем опустить, поскольку одно действие
-//     }
-//     return sum;
-// }
+int SumNumbers(int num) // int num = number
+{
+    int sum = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        sum = sum + i; // sum += i/ Хотя фигурные скобки '{}' можем опустить, поскольку одно действие
+    }
+    return sum;
+}
 
 
 //                  Игорь
