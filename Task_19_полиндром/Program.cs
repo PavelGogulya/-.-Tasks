c7ce1ebad596b13f190168506027c4fb07cb3a79
PavelGogulya <PavelGogulya@example.com>
2023-04-10 06:19:58 +0300

    в задаче 19 Нужно допилить блок проверки на 5ти значность числа

--- Task_19_полиндром/Program.cs
+++ Task_19_полиндром/Program.cs
@@ -4,17 +4,21 @@
 Console.WriteLine("Введите пятизначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
+//if (10000 < number < 99999 )
+
+//{
 int a1 = number % 10;
 int a2 = number / 10 % 10;
 int a3 = number / 100 % 10;
 int a4 = number / 1000 % 10;
 int a5 = number / 10000;
 int pSum = a1 * 10000 + a2 * 1000 + a3 * 100 + a4 * 10 + a5;
-
+//}
 if( number == pSum)
-
 {
 Console.Write($"{number} есть полиндром");
 }
+
 else
-Console.WriteLine($"Число {number} НЕ полиндромом!");
+Console.WriteLine($"Число {number} НЕ является полиндромом!");
+
