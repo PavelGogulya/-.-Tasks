fb9f7ce2029f822dc37a76b27f11a2af7f954365
PavelGogulya <PavelGogulya@example.com>
2023-04-12 05:36:50 +0300

    Добавил комментарии к программе задачи 00

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -6,13 +6,15 @@
 -7 -> 49 */
 
 
-Console.WriteLine("Введите целое число");
+Console.WriteLine("Введите целое число"); // Приглашение от программы на ввод данных
 
-int number = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine()); // Данный оператор 'Console.ReadLine()' строчного типа,
+// поэтому зачастую его преобразуем. Это делается оператором 'Convert.ToInt32'
 
 /* ЛИБО string str = Console.ReadLine();
 int number = Convert.ToInt32(str); */
 
 int squareNumber = number * number;
-Console.WriteLine($"Квадрат числа {number} = {squareNumber}");
+Console.WriteLine($"Квадрат числа {number} = {squareNumber}"); //  знак '$' есть интерполяция строк. 
+//т.е в '{}' фигурных скобках выводится ЗНАЧЕНИЕ пременных а не их название как текст
 
