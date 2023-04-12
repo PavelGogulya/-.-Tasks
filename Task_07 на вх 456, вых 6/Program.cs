e0d2a3489d0b77b2a9ead1ae170edeb7a707b3bf
PavelGogulya <PavelGogulya@example.com>
2023-04-12 10:43:31 +0300

    Создали программу для Task_07

---
+++ Task_07 на вх 456, вых 6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 7
+// Прога которая выдает последнюю цифру от введнного пользователем 3х значного числа. 
+// 456 -> 6
+// 782 -> 2
+
+Console.WriteLine("введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999) // Условие проверки трехзначности
+{
+    int lastNumber = number %10;
+    Console.WriteLine($"последняя цифра числа {lastNumber}"); //вычисление работает 
+}
+else
+Console.WriteLine ($" число {number} не трехзначное!");
